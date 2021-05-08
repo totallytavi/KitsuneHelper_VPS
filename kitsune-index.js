04651376a55d94046dc78c1430cbe91dc68594eb
Tavi <>
2021-05-08 06:31:29 +0000

    A VERY TEENY TINY SPELLING MISTAKE

--- kitsune-index.js
+++ kitsune-index.js
@@ -166,13 +166,13 @@ if(tags.username.toLowerCase() === "coder_tavi") { // Owner only commands
     fs.writeFile("allowViewers.txt", "true", () => {
       bot.say(channel, `@${tags.username}, viewers are now allowed to join games!`)
     })
-    client.channels.cache.get('840448163998728193').send("Stream updated to: `allowvViewers = true` by `" + tags.username + "`")
+    client.channels.cache.get('840448163998728193').send("Stream updated to `allowViewers = true` by `" + tags.username + "`")
   }
   if(message.toLowerCase() === "!declineviewers") {
     fs.writeFile("allowViewers.txt", "false", () => {
       bot.say(channel, `@${tags.username}, viewers are now not allowed to join games!`)
     })
-    client.channels.cache.get('840448163998728193').send("Stream updated to `allowvViewers = false` by `" + tags.username + "`")
+    client.channels.cache.get('840448163998728193').send("Stream updated to `allowViewers = false` by `" + tags.username + "`")
   }
   if(message.toLowerCase().startsWith() === "!eval") {
     await eval(message.toLowerCase().slice(6))
