d3df6ed32808f1a529f23bd35b99ec3846a5e54f
Tavi <66774833+Coder-Tavi@users.noreply.github.com>
2021-06-10 05:31:12 -0700

    Disable case-sensitive prefix

--- kitsune-index.js
+++ kitsune-index.js
@@ -83,7 +83,7 @@ client.on("message", async message => {
 
     if (message.author.bot) return;
     if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.toLowerCase().startsWith(prefix)) return;
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
