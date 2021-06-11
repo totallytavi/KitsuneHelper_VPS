7f907cbddc0bbe154700291cc6ab8d5cc627fe36
Tavi <66774833+Coder-Tavi@users.noreply.github.com>
2021-06-11 01:36:50 -1000

    v1.1.2 (Pure)
    Officially released the version!

--- functions.js
+++ functions.js
@@ -226,24 +226,18 @@ module.exports = {
        * @example toConsole("index.js (Line 69)", "We hit an error!", '', client)
        */
       toConsole: async function(reason, source, message, client) {
-        console.log(0)
         if(!reason) return new SyntaxError("reason is a required argument");
         if(typeof reason != 'string') return new SyntaxError("reason is not a string");
-        console.log(1)
         if(!source) return new SyntaxError("source is a required argument");
         if(typeof source != 'string') return new SyntaxError("source is not a string");
-        console.log(2)
         if(!client) return new SyntaxError("client is a required argument")
         if(typeof client != 'object') return new SyntaxError("client is not an object");
 
-        console.log(3)
         if(typeof client.channels.cache.get('775560270700347432') != 'object') return console.log("Error channel was not found; aborting...")
 
-        console.log(4)
         const embed = new MessageEmbed()
         switch(typeof message.content) {
           case 'string':
-            console.log(5)
             embed
             .setTitle("Message to Console")
             .setColor("RED")
@@ -264,7 +258,6 @@ module.exports = {
 
             break;
           default:
-            console.log(6)
             embed
             .setTitle("Message to Console")
             .setColor("RED")
