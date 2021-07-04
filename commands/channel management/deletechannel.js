4024e4af6fa245d81bf6e66d77adc1ad4b298a1f
Tavi <66774833+Coder-Tavi@users.noreply.github.com>
2021-07-03 19:00:51 -0500

    New packages (No update being added)

--- commands/channel management/deletechannel.js
+++ commands/channel management/deletechannel.js
@@ -1,4 +1,4 @@
-const { toConsole, responseEmbed } = require("../../functions");
+const { toConsole, responseEmbed, promptMessage } = require("../../functions");
 const cooldown = new Set();
 const auth = new Set();
 
@@ -32,9 +32,28 @@ module.exports = {
 
       if(!toDelete) return responseEmbed(3, "Not Found: No channel found for " + toDelete)
 
-      toDelete.delete({ reason: `Moderator: ${message.author.tag}` })
+      toDelete.delete({ reason: `Moderator: ${message.author.tag} (Moderator: ${message.author.id})` })
         .then(responseEmbed(1, "The channel was deleted", "CHANNEL", message, client))
         .catch(e => toConsole(String(e), "deletechannel.js (Line 35)", message, client))
+
+      // Make the filter, this is needed later
+      const filter = m => m.author === message.author;
+
+      if(toDelete.type === 'category') {
+      message.channel.send("I detected the channel as a category, should I delete the category's channels?\n\n> *This will expire in 30 seconds*")
+      .then(m => {
+        const emoji = await promptMessage(m, message.author, 30, ["✅", "❌"])
+
+        if(emoji === "✅") {
+          message.guild.channels.cache.each(channel => {
+            if(channel.parent != toDelete) return;
+            if(!channel.manageable) return responseEmbed(1, "Unauthorized: I am not authorized to delete " + channel)
+
+            channel.delete({ reason: `Moderator ${message.author.tag} (${message.author.id})`})
+          })
+        }
+      })
+      }
 
       auth.delete(message.author.id);
       cooldown.add(message.author.id);
